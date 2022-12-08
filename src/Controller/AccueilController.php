c3814e90fe7e2875ff7c27c73e87f3241f3997f7
louise <louisepop14@gmail.com>
2022-12-08 13:29:32 +0100

    Phase 22 : codage de la page d'accueil du site.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,21 +1,39 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
  * Description of AccueilController
  *
  * @author emds
  */
-class AccueilController {
+class AccueilController extends AbstractController{
+
+    /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
 
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
     public function index(): Response{
-        return new Response('Hello world !');
+        $visites = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueil.html.twig", [
+            'visites' => $visites
+        ]);   
     }
 }
